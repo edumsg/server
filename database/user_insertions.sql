1dd22c1894890dd0dcb8266a7e17261c8573cef2
elhagin <omar.elhagin@live.com>
2016-02-29 20:47:26 +0200

    modified generator to match schema modifications and increased row count

--- database/user_insertions.sql
+++ database/user_insertions.sql
@@ -1,19 +1,39 @@
-INSERT INTO users VALUES(DEFAULT,'sara', 'a0@a.com', '$2a$10$LWTMQA4F1.jqctvsJtFapu7X.wTo8lXhLybg5.7haAF2Vt7k7DKIG','Sara', 'english', 'egypt',  'fsdfdsjlfdlkjdfk', 'www.google.com', CURRENT_TIMESTAMP, 'http://bit.ly/20VGjpB', true, '#FF0000', '#00FF00', false, NULL );
+INSERT INTO users VALUES(DEFAULT,'sara', 'a0@a.com', '$2a$10$LWTMQA4F1.jqctvsJtFapu7X.wTo8lXhLybg5.7haAF2Vt7k7DKIG','Sara', '23/7/1984', 'female', 'english', 'egypt',  'fsdfdsjlfdlkjdfk', 'www.google.com', CURRENT_TIMESTAMP, 'http://bit.ly/20VGjpB', true, '#FF0000', '#00FF00', false, NULL );
 
-INSERT INTO users VALUES(DEFAULT,'magda', 'a1@a.com', '$2a$10$LWTMQA4F1.jqctvsJtFapu7X.wTo8lXhLybg5.7haAF2Vt7k7DKIG','Magda', 'english', 'egypt',  'fsdfdsjlfdlkjdfk', 'www.google.com', CURRENT_TIMESTAMP, 'http://bit.ly/20VGjpB', true, '#FF0000', '#00FF00', false, NULL );
+INSERT INTO users VALUES(DEFAULT,'magda', 'a1@a.com', '$2a$10$LWTMQA4F1.jqctvsJtFapu7X.wTo8lXhLybg5.7haAF2Vt7k7DKIG','Magda', '23/7/1984', 'female', 'english', 'egypt',  'fsdfdsjlfdlkjdfk', 'www.google.com', CURRENT_TIMESTAMP, 'http://bit.ly/20VGjpB', true, '#FF0000', '#00FF00', false, NULL );
 
-INSERT INTO users VALUES(DEFAULT,'sameer', 'a2@a.com', '$2a$10$LWTMQA4F1.jqctvsJtFapu7X.wTo8lXhLybg5.7haAF2Vt7k7DKIG','Sameer', 'english', 'egypt',  'fsdfdsjlfdlkjdfk', 'www.google.com', CURRENT_TIMESTAMP, 'http://bit.ly/20VGjpB', true, '#FF0000', '#00FF00', false, NULL );
+INSERT INTO users VALUES(DEFAULT,'sameer', 'a2@a.com', '$2a$10$LWTMQA4F1.jqctvsJtFapu7X.wTo8lXhLybg5.7haAF2Vt7k7DKIG','Sameer', '23/7/1984', 'male', 'english', 'egypt',  'fsdfdsjlfdlkjdfk', 'www.google.com', CURRENT_TIMESTAMP, 'http://bit.ly/20VGjpB', true, '#FF0000', '#00FF00', false, NULL );
 
-INSERT INTO users VALUES(DEFAULT,'sameh', 'a3@a.com', '$2a$10$LWTMQA4F1.jqctvsJtFapu7X.wTo8lXhLybg5.7haAF2Vt7k7DKIG','Sameh', 'english', 'egypt',  'fsdfdsjlfdlkjdfk', 'www.google.com', CURRENT_TIMESTAMP, 'http://bit.ly/20VGjpB', true, '#FF0000', '#00FF00', false, NULL );
+INSERT INTO users VALUES(DEFAULT,'sameh', 'a3@a.com', '$2a$10$LWTMQA4F1.jqctvsJtFapu7X.wTo8lXhLybg5.7haAF2Vt7k7DKIG','Sameh', '23/7/1984', 'male', 'english', 'egypt',  'fsdfdsjlfdlkjdfk', 'www.google.com', CURRENT_TIMESTAMP, 'http://bit.ly/20VGjpB', true, '#FF0000', '#00FF00', false, NULL );
 
-INSERT INTO users VALUES(DEFAULT,'samar', 'a4@a.com', '$2a$10$LWTMQA4F1.jqctvsJtFapu7X.wTo8lXhLybg5.7haAF2Vt7k7DKIG','Samar', 'english', 'egypt',  'fsdfdsjlfdlkjdfk', 'www.google.com', CURRENT_TIMESTAMP, 'http://bit.ly/20VGjpB', true, '#FF0000', '#00FF00', false, NULL );
+INSERT INTO users VALUES(DEFAULT,'samar', 'a4@a.com', '$2a$10$LWTMQA4F1.jqctvsJtFapu7X.wTo8lXhLybg5.7haAF2Vt7k7DKIG','Samar', '23/7/1984', 'female', 'english', 'egypt',  'fsdfdsjlfdlkjdfk', 'www.google.com', CURRENT_TIMESTAMP, 'http://bit.ly/20VGjpB', true, '#FF0000', '#00FF00', false, NULL );
 
-INSERT INTO users VALUES(DEFAULT,'hadeel', 'a5@a.com', '$2a$10$LWTMQA4F1.jqctvsJtFapu7X.wTo8lXhLybg5.7haAF2Vt7k7DKIG','Hadeel', 'english', 'egypt',  'fsdfdsjlfdlkjdfk', 'www.google.com', CURRENT_TIMESTAMP, 'http://bit.ly/20VGjpB', true, '#FF0000', '#00FF00', false, NULL );
+INSERT INTO users VALUES(DEFAULT,'hadeel', 'a5@a.com', '$2a$10$LWTMQA4F1.jqctvsJtFapu7X.wTo8lXhLybg5.7haAF2Vt7k7DKIG','Hadeel', '23/7/1984', 'female', 'english', 'egypt',  'fsdfdsjlfdlkjdfk', 'www.google.com', CURRENT_TIMESTAMP, 'http://bit.ly/20VGjpB', true, '#FF0000', '#00FF00', false, NULL );
 
-INSERT INTO users VALUES(DEFAULT,'samer', 'a6@a.com', '$2a$10$LWTMQA4F1.jqctvsJtFapu7X.wTo8lXhLybg5.7haAF2Vt7k7DKIG','Samer', 'english', 'egypt',  'fsdfdsjlfdlkjdfk', 'www.google.com', CURRENT_TIMESTAMP, 'http://bit.ly/20VGjpB', true, '#FF0000', '#00FF00', false, NULL );
+INSERT INTO users VALUES(DEFAULT,'samer', 'a6@a.com', '$2a$10$LWTMQA4F1.jqctvsJtFapu7X.wTo8lXhLybg5.7haAF2Vt7k7DKIG','Samer', '23/7/1984', 'male', 'english', 'egypt',  'fsdfdsjlfdlkjdfk', 'www.google.com', CURRENT_TIMESTAMP, 'http://bit.ly/20VGjpB', true, '#FF0000', '#00FF00', false, NULL );
 
-INSERT INTO users VALUES(DEFAULT,'medhat', 'a7@a.com', '$2a$10$LWTMQA4F1.jqctvsJtFapu7X.wTo8lXhLybg5.7haAF2Vt7k7DKIG','Medhat', 'english', 'egypt',  'fsdfdsjlfdlkjdfk', 'www.google.com', CURRENT_TIMESTAMP, 'http://bit.ly/20VGjpB', true, '#FF0000', '#00FF00', false, NULL );
+INSERT INTO users VALUES(DEFAULT,'medhat', 'a7@a.com', '$2a$10$LWTMQA4F1.jqctvsJtFapu7X.wTo8lXhLybg5.7haAF2Vt7k7DKIG','Medhat', '23/7/1984', 'male', 'english', 'egypt',  'fsdfdsjlfdlkjdfk', 'www.google.com', CURRENT_TIMESTAMP, 'http://bit.ly/20VGjpB', true, '#FF0000', '#00FF00', false, NULL );
 
-INSERT INTO users VALUES(DEFAULT,'salma', 'a8@a.com', '$2a$10$LWTMQA4F1.jqctvsJtFapu7X.wTo8lXhLybg5.7haAF2Vt7k7DKIG','Salma', 'english', 'egypt',  'fsdfdsjlfdlkjdfk', 'www.google.com', CURRENT_TIMESTAMP, 'http://bit.ly/20VGjpB', true, '#FF0000', '#00FF00', false, NULL );
+INSERT INTO users VALUES(DEFAULT,'salma', 'a8@a.com', '$2a$10$LWTMQA4F1.jqctvsJtFapu7X.wTo8lXhLybg5.7haAF2Vt7k7DKIG','Salma', '23/7/1984', 'female', 'english', 'egypt',  'fsdfdsjlfdlkjdfk', 'www.google.com', CURRENT_TIMESTAMP, 'http://bit.ly/20VGjpB', true, '#FF0000', '#00FF00', false, NULL );
 
-INSERT INTO users VALUES(DEFAULT,'sondos', 'a9@a.com', '$2a$10$LWTMQA4F1.jqctvsJtFapu7X.wTo8lXhLybg5.7haAF2Vt7k7DKIG','Sondos', 'english', 'egypt',  'fsdfdsjlfdlkjdfk', 'www.google.com', CURRENT_TIMESTAMP, 'http://bit.ly/20VGjpB', true, '#FF0000', '#00FF00', false, NULL );
+INSERT INTO users VALUES(DEFAULT,'sondos', 'a9@a.com', '$2a$10$LWTMQA4F1.jqctvsJtFapu7X.wTo8lXhLybg5.7haAF2Vt7k7DKIG','Sondos', '23/7/1984', 'female', 'english', 'egypt',  'fsdfdsjlfdlkjdfk', 'www.google.com', CURRENT_TIMESTAMP, 'http://bit.ly/20VGjpB', true, '#FF0000', '#00FF00', false, NULL );
+
+INSERT INTO users VALUES(DEFAULT,'omar', 'a10@a.com', '$2a$10$LWTMQA4F1.jqctvsJtFapu7X.wTo8lXhLybg5.7haAF2Vt7k7DKIG','Omar', '23/7/1984', 'male', 'english', 'egypt',  'fsdfdsjlfdlkjdfk', 'www.google.com', CURRENT_TIMESTAMP, 'http://bit.ly/20VGjpB', true, '#FF0000', '#00FF00', false, NULL );
+
+INSERT INTO users VALUES(DEFAULT,'mohamed', 'a11@a.com', '$2a$10$LWTMQA4F1.jqctvsJtFapu7X.wTo8lXhLybg5.7haAF2Vt7k7DKIG','Mohamed', '23/7/1984', 'male', 'english', 'egypt',  'fsdfdsjlfdlkjdfk', 'www.google.com', CURRENT_TIMESTAMP, 'http://bit.ly/20VGjpB', true, '#FF0000', '#00FF00', false, NULL );
+
+INSERT INTO users VALUES(DEFAULT,'amgad', 'a12@a.com', '$2a$10$LWTMQA4F1.jqctvsJtFapu7X.wTo8lXhLybg5.7haAF2Vt7k7DKIG','Amgad', '23/7/1984', 'male', 'english', 'egypt',  'fsdfdsjlfdlkjdfk', 'www.google.com', CURRENT_TIMESTAMP, 'http://bit.ly/20VGjpB', true, '#FF0000', '#00FF00', false, NULL );
+
+INSERT INTO users VALUES(DEFAULT,'menna', 'a13@a.com', '$2a$10$LWTMQA4F1.jqctvsJtFapu7X.wTo8lXhLybg5.7haAF2Vt7k7DKIG','Menna', '23/7/1984', 'female', 'english', 'egypt',  'fsdfdsjlfdlkjdfk', 'www.google.com', CURRENT_TIMESTAMP, 'http://bit.ly/20VGjpB', true, '#FF0000', '#00FF00', false, NULL );
+
+INSERT INTO users VALUES(DEFAULT,'lamees', 'a14@a.com', '$2a$10$LWTMQA4F1.jqctvsJtFapu7X.wTo8lXhLybg5.7haAF2Vt7k7DKIG','Lamees', '23/7/1984', 'female', 'english', 'egypt',  'fsdfdsjlfdlkjdfk', 'www.google.com', CURRENT_TIMESTAMP, 'http://bit.ly/20VGjpB', true, '#FF0000', '#00FF00', false, NULL );
+
+INSERT INTO users VALUES(DEFAULT,'farah', 'a15@a.com', '$2a$10$LWTMQA4F1.jqctvsJtFapu7X.wTo8lXhLybg5.7haAF2Vt7k7DKIG','Farah', '23/7/1984', 'female', 'english', 'egypt',  'fsdfdsjlfdlkjdfk', 'www.google.com', CURRENT_TIMESTAMP, 'http://bit.ly/20VGjpB', true, '#FF0000', '#00FF00', false, NULL );
+
+INSERT INTO users VALUES(DEFAULT,'ismail', 'a16@a.com', '$2a$10$LWTMQA4F1.jqctvsJtFapu7X.wTo8lXhLybg5.7haAF2Vt7k7DKIG','Ismail', '23/7/1984', 'male', 'english', 'egypt',  'fsdfdsjlfdlkjdfk', 'www.google.com', CURRENT_TIMESTAMP, 'http://bit.ly/20VGjpB', true, '#FF0000', '#00FF00', false, NULL );
+
+INSERT INTO users VALUES(DEFAULT,'magdy', 'a17@a.com', '$2a$10$LWTMQA4F1.jqctvsJtFapu7X.wTo8lXhLybg5.7haAF2Vt7k7DKIG','Magdy', '23/7/1984', 'male', 'english', 'egypt',  'fsdfdsjlfdlkjdfk', 'www.google.com', CURRENT_TIMESTAMP, 'http://bit.ly/20VGjpB', true, '#FF0000', '#00FF00', false, NULL );
+
+INSERT INTO users VALUES(DEFAULT,'amal', 'a18@a.com', '$2a$10$LWTMQA4F1.jqctvsJtFapu7X.wTo8lXhLybg5.7haAF2Vt7k7DKIG','Amal', '23/7/1984', 'female', 'english', 'egypt',  'fsdfdsjlfdlkjdfk', 'www.google.com', CURRENT_TIMESTAMP, 'http://bit.ly/20VGjpB', true, '#FF0000', '#00FF00', false, NULL );
+
+INSERT INTO users VALUES(DEFAULT,'laila', 'a19@a.com', '$2a$10$LWTMQA4F1.jqctvsJtFapu7X.wTo8lXhLybg5.7haAF2Vt7k7DKIG','Laila', '23/7/1984', 'female', 'english', 'egypt',  'fsdfdsjlfdlkjdfk', 'www.google.com', CURRENT_TIMESTAMP, 'http://bit.ly/20VGjpB', true, '#FF0000', '#00FF00', false, NULL );
--- insertion generator.pl6
+++ insertion generator.pl6
@@ -1,10 +1,12 @@
-our @names = ["Sara","Magda", "Sameer", "Sameh", "Samar", "Hadeel", "Samer", "Medhat","Salma","Sondos"];
+our @names = ["Sara","Magda", "Sameer", "Sameh", "Samar", "Hadeel", "Samer", "Medhat","Salma","Sondos","Omar","Mohamed","Amgad","Menna","Lamees","Farah","Ismail","Magdy","Amal","Laila"];
 
-our %ids = (Sara => 1, Magda => 2, Sameer => 3, Sameh => 4, Samar => 5, Hadeel => 6, Samer => 7, Medhat => 8, Salma => 9, Sondos => 10);
+our @genders = ["female", "female", "male", "male", "female", "female", "male", "male", "female", "female", "male", "male", "male", "female", "female", "female", "male", "male", "female", "female"];
+
+our %ids = (Sara => 1, Magda => 2, Sameer => 3, Sameh => 4, Samar => 5, Hadeel => 6, Samer => 7, Medhat => 8, Salma => 9, Sondos => 10, Omar => 11, Mohamed => 12, Amgad => 13, Menna => 14, Lamees => 15, Farah => 16, Ismail => 17, Magdy => 18, Amal => 19, Laila => 20);
 
 sub user_insertions{
-    for 0..9 -> $i {
-        say "INSERT INTO users VALUES(DEFAULT,'"  ~@names[$i].gist.lc~ "', 'a$i@a.com', '\$2a\$10\$LWTMQA4F1.jqctvsJtFapu7X.wTo8lXhLybg5.7haAF2Vt7k7DKIG','" ~@names[$i].gist~ "', 'english', 'egypt',  'fsdfdsjlfdlkjdfk', 'www.google.com', CURRENT_TIMESTAMP, 'http://bit.ly/20VGjpB', true, '#FF0000', '#00FF00', false, NULL );\n";
+    for 0..19 -> $i {
+        say "INSERT INTO users VALUES(DEFAULT,'"  ~@names[$i].gist.lc~ "', 'a$i@a.com', '\$2a\$10\$LWTMQA4F1.jqctvsJtFapu7X.wTo8lXhLybg5.7haAF2Vt7k7DKIG','" ~@names[$i].gist~ "', '23/7/1984', '" ~@genders[$i].gist~ "', 'english', 'egypt',  'fsdfdsjlfdlkjdfk', 'www.google.com', CURRENT_TIMESTAMP, 'http://bit.ly/20VGjpB', true, '#FF0000', '#00FF00', false, NULL );\n";
     }
 }
 
@@ -20,8 +22,8 @@ sub tweets_insertions{
 
 sub followships_insertions{
     my @temp;
-    for 1..10 -> $i {
-        for 1..10 -> $j {
+    for 1..20 -> $i {
+        for 1..20 -> $j {
             next if ($i == $j);
             @temp.push("INSERT INTO followships VALUES(DEFAULT, $i, $j, true, CURRENT_TIMESTAMP);");
             @temp.push("INSERT INTO followships VALUES(DEFAULT, $j, $i, true, CURRENT_TIMESTAMP);");
@@ -32,28 +34,22 @@ sub followships_insertions{
 
 sub dm_insertions{
     my @temp;
-    my $counter = 1;
-    for 1..10 -> $i {
-        for 1..10 -> $j {
+    for 1..11 -> $i {
+        for 1..11 -> $j {
             next if ($i == $j);
             @temp.push("INSERT INTO conversations VALUES(DEFAULT, $i, $j);");
-            $counter++;
 
-            @temp.push("INSERT INTO direct_messages VALUES(DEFAULT, $i, $j, 'Hello', NULL, true, $counter, CURRENT_TIMESTAMP);");
+            @temp.push("INSERT INTO direct_messages VALUES(DEFAULT, $i, $j, 'Hello', NULL, true, CURRVAL(pg_get_serial_sequence('conversations','id')), CURRENT_TIMESTAMP);");
 
-            @temp.push("INSERT INTO direct_messages VALUES(DEFAULT, $j, $i, 'Hello', NULL, true, $counter, CURRENT_TIMESTAMP);");
+            @temp.push("INSERT INTO direct_messages VALUES(DEFAULT, $j, $i, 'Hello', NULL, true, CURRVAL(pg_get_serial_sequence('conversations','id')), CURRENT_TIMESTAMP);");
 
         }
     }
-    @temp.unique>>.say;
+    @temp>>.say;
 }
 
 sub replies_insertions{
-    my $c= 1;
-    for 1..10 {
-        say "INSERT INTO replies VALUES(DEFAULT, $c, " ~$c+20~ ", CURRENT_TIMESTAMP);";
-        $c+=20;
+    for 0..19 -> $i {
+        say "INSERT INTO replies VALUES(DEFAULT, (SELECT id FROM tweets LIMIT 1 OFFSET " ~$i~ "), (SELECT id FROM tweets LIMIT 1 OFFSET " ~$i+1~ "), CURRENT_TIMESTAMP);";
     }
 }
-
-replies_insertions;
