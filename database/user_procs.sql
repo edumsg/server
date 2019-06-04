b250ee206489340ffd8a6342c81c7565dd2b3d0b
Hazem Hisham <hazemhisham98@hotmail.com>
2019-06-04 15:00:38 +0200

    userID from session fix

--- database/user_procs.sql
+++ database/user_procs.sql
@@ -29,7 +29,7 @@ BEGIN
     SELECT user_id
     INTO userID
     FROM sessions
-    WHERE id = $1 AND session_end <> NULL;
+    WHERE id = $1;
 
     IF NOT FOUND THEN
         RAISE EXCEPTION 'no such session exists';
